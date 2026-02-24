4996e3ad2c956434ff8bd6a0fc843209fa4cbd21
melika <melikafarahani456@gmail.com>
2026-02-24 10:45:59 +0330

    Enhance routing and styling: add create task route, improve CSS for better layout and responsiveness

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import Dashboard from '../views/Dashboard.vue';
+import CreateTask from '../views/createTask.vue';
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
   { path: '/register', component: Register },
+  { path: '/create-task', component: CreateTask },
   { path: '/dashboard', component: Dashboard },
 ];
 
@@ -18,7 +20,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('token')
 
-  if (to.path === '/dashboard' && !token) {
+  if ((to.path === '/dashboard' || to.path === '/create-task') && !token) {
     next('/login')
   } else {
     next()
